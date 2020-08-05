3acaada5109a4aef2525876d243157bedcbec910
Shaktijeet <shaktijeet.tripathi@gmail.com>
2020-08-05 15:30:37 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <script type="text/javascript" src="js/jssor.slider-28.0.0.min.js"></script>
+
+</head>
+<body>
+    
+</body>
+</html>
